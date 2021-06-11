c36399e2b8b0332b986a37c36f9b5a0561f94fdc
p-ml <34091508+pjjml@users.noreply.github.com>
2021-06-11 19:04:16 +0100

    Initial commit

---
+++ fantasy_funball/unit_test/mock_test.py
@@ -0,0 +1,8 @@
+from unittest import TestCase
+
+""" Unit tests go here """
+
+
+class TestMock(TestCase):
+    def test_mock(self):
+        self.assertTrue(True)
---
+++ functional_test/mock_test.py
@@ -0,0 +1,8 @@
+from unittest import TestCase
+
+""" Functional tests go here """
+
+
+class TestMock(TestCase):
+    def test_mock(self):
+        self.assertTrue(True)
