2785da3e4b0a1db021f8d79a05617bf00d019ab8
p-ml <34091508+pjjml@users.noreply.github.com>
2021-06-13 18:35:45 +0100

    Draft view for retrieving fixture data

--- fantasy_funball/urls.py
+++ fantasy_funball/urls.py
@@ -16,11 +16,18 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from fantasy_funball.views.funballer import (CreateFunballerView,
-                                             ModifyFunballerView)
+from fantasy_funball.views.fixtures import (
+    RetrieveFixture,
+    RetrieveGameday,
+    RetrieveGameweek,
+)
+from fantasy_funball.views.funballer import CreateFunballerView, ModifyFunballerView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("funballer/<int:id>", ModifyFunballerView.as_view(), name="funballer-detail"),
-    path("funballer/", CreateFunballerView.as_view(), name="create-funballer")
+    path("funballer/", CreateFunballerView.as_view(), name="create-funballer"),
+    path("fixture/<int:id>", RetrieveFixture.as_view(), name="retrieve-fixture"),
+    path("gameday/<int:id>", RetrieveGameday.as_view(), name="retrieve-gameday"),
+    path("gameweek/<int:id>", RetrieveGameweek.as_view(), name="retrieve-gameweek"),
 ]
---
+++ fantasy_funball/views/fixtures.py
@@ -0,0 +1,70 @@
+from django.core.handlers.wsgi import WSGIRequest
+from django.forms import model_to_dict
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from fantasy_funball.models import Fixture, Gameday, Gameweek
+
+
+class RetrieveFixture(APIView):
+    """Viewset to handle fixtures"""
+
+    def get(self, request: WSGIRequest, id: int) -> Response:
+        """Retrieve fixture from MongoDB"""
+        # TODO: Might want to do this by team name and date
+
+        try:
+            fixture = Fixture.objects.get(id=id)
+        except Fixture.DoesNotExist:
+            return Response(status=404)
+
+        # Convert to json for output
+        formatted_fixture = model_to_dict(fixture)
+
+        return Response(
+            status=status.HTTP_200_OK,
+            data=formatted_fixture,
+        )
+
+
+class RetrieveGameday(APIView):
+    """Viewset to handle gamedays"""
+
+    def get(self, request: WSGIRequest, id: int) -> Response:
+        """Retrieve gameday from MongoDB"""
+        # TODO: Might want to do this by date
+
+        try:
+            gameday = Gameday.objects.get(id=id)
+        except Gameday.DoesNotExist:
+            return Response(status=404)
+
+        # Convert to json for output
+        formatted_gameday = model_to_dict(gameday)
+
+        return Response(
+            status=status.HTTP_200_OK,
+            data=formatted_gameday,
+        )
+
+
+class RetrieveGameweek(APIView):
+    """Viewset to handle gameweeks"""
+
+    def get(self, request: WSGIRequest, id: int) -> Response:
+        """Retrieve gameweek from MongoDB"""
+        # TODO: Might want to do this by gameweek no. (1-38)
+
+        try:
+            gameweek = Gameweek.objects.get(id=id)
+        except Gameweek.DoesNotExist:
+            return Response(status=404)
+
+        # Convert to json for output
+        formatted_gameweek = model_to_dict(gameweek)
+
+        return Response(
+            status=status.HTTP_200_OK,
+            data=formatted_gameweek,
+        )
