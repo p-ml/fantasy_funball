dc3b4bf53420773ada958a4a302c401b2e8ab3d8
p-ml <34091508+pjjml@users.noreply.github.com>
2021-07-25 17:21:19 +0100

    Fix player and fixture endpoints

--- fantasy_funball/tests/test_views/test_player.py
+++ fantasy_funball/tests/test_views/test_player.py
@@ -7,23 +7,18 @@ from rest_framework import status
 
 django.setup()
 
-from fantasy_funball.models import Player, Team
+from fantasy_funball.models import Team
+
+PLAYER_VIEW_PATH = "fantasy_funball.views.player"
 
 
 class TestPlayerViewSet(TestCase):
     def setUp(self) -> None:
         self.client = Client()
 
-    @patch("fantasy_funball.views.player.Player.objects.filter")
-    def test_get(self, mock_retrieve_player):
-        mock_retrieve_player.return_value = [
-            Player(
-                id=1,
-                first_name="Hugo",
-                surname="Lloris",
-                team_id=1,
-            )
-        ]
+    @patch(f"{PLAYER_VIEW_PATH}.Result.objects.filter")
+    @patch(f"{PLAYER_VIEW_PATH}.Player.objects.filter")
+    def test_get(self, mock_retrieve_player, mock_retrieve_result):
         expected_output = [
             {
                 "first_name": "Hugo",
@@ -32,11 +27,16 @@ class TestPlayerViewSet(TestCase):
                 "id": 1,
             }
         ]
+
+        mock_retrieve_result.return_value.count.return_value = 0
+
+        mock_retrieve_player.return_value.values.return_value = expected_output
+
         response = self.client.get("/fantasy_funball/spurs/players/")
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data, expected_output)
 
-    @patch("fantasy_funball.views.player.Player.objects.filter")
+    @patch(f"{PLAYER_VIEW_PATH}.Player.objects.filter")
     def test_get_team_not_found(self, mock_retrieve_player):
         mock_retrieve_player.side_effect = Team.DoesNotExist
         invalid_team_name = "barcelona"
--- fantasy_funball/views/player.py
+++ fantasy_funball/views/player.py
@@ -1,23 +1,28 @@
 from django.core.handlers.wsgi import WSGIRequest
-from django.forms import model_to_dict
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from core.exceptions import TeamNotFoundError
-from fantasy_funball.models import Player, Team
+from fantasy_funball.models import Player, Result, Team
 
 
 class PlayerTeamView(APIView):
     def get(self, request: WSGIRequest, team_name: str) -> Response:
         try:
-            team_players = Player.objects.filter(
-                team__team_name=team_name,
+            team_players = Player.objects.filter(team__team_name=team_name,).values(
+                "id",
+                "first_name",
+                "surname",
             )
         except Team.DoesNotExist:
             raise TeamNotFoundError(f"{team_name} not found")
 
-        formatted_team_players = [model_to_dict(player) for player in team_players]
+        for player in team_players:
+            player["goals"] = Result.objects.filter(scorers=player["id"]).count()
+            player["assists"] = Result.objects.filter(assists=player["id"]).count()
+
+        formatted_team_players = list(team_players)
 
         return Response(
             status=status.HTTP_200_OK,
