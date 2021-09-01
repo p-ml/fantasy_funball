d3e2dfed2118e1fb8b4499105193f82cdb1a530a
p-ml <34091508+pjjml@users.noreply.github.com>
2021-09-01 18:25:14 +0100

    Fix models import order to avoid circular import

--- fantasy_funball/models/__init__.py
+++ fantasy_funball/models/__init__.py
@@ -1,8 +1,8 @@
 from .teams import Team  # isort:skip
 from .players import Player  # isort:skip
 from .fixtures import Gameweek, Fixture, Gameday, Result  # isort:skip
+from .funballer import Funballer  # isort:skip
 from .choices import Choices
-from .funballer import Funballer
 from .misc import GameweekSummary
 
 __all__ = [
