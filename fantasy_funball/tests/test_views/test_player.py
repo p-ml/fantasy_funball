5959258f531dacf56a70e59310d03fc5da195715
p-ml <34091508+pjjml@users.noreply.github.com>
2021-09-18 11:19:03 +0100

    Fix player view UT

--- fantasy_funball/tests/test_views/test_player.py
+++ fantasy_funball/tests/test_views/test_player.py
@@ -16,8 +16,15 @@ class TestPlayerTeamViewSet(TestCase):
     def setUp(self) -> None:
         self.client = Client()
 
+    @patch(f"{PLAYER_VIEW_PATH}.Assists.objects.filter")
+    @patch(f"{PLAYER_VIEW_PATH}.Goals.objects.filter")
     @patch(f"{PLAYER_VIEW_PATH}.Player.objects.filter")
-    def test_get(self, mock_retrieve_player):
+    def test_get(
+        self,
+        mock_retrieve_player,
+        mock_retrieve_goals,
+        mock_retrieve_assists,
+    ):
         expected_output = [
             {
                 "first_name": "Hugo",
@@ -28,6 +35,13 @@ class TestPlayerTeamViewSet(TestCase):
         ]
 
         mock_retrieve_player.return_value.values.return_value = expected_output
+
+        mock_retrieve_goals.return_value.aggregate.return_value = {
+            "goals_scored__sum": 1,
+        }
+        mock_retrieve_assists.return_value.aggregate.return_value = {
+            "assists_made__sum": 1,
+        }
 
         response = self.client.get("/fantasy_funball/spurs/players/")
         self.assertEqual(response.status_code, status.HTTP_200_OK)
