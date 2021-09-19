e403921872d0b025337abaa53db08a47b80a2363
p-ml <34091508+pjjml@users.noreply.github.com>
2021-09-19 12:22:36 +0100

    Add restore results script

---
+++ fantasy_funball/dev_tools/scripts/restore_results.py
@@ -0,0 +1,8 @@
+from fantasy_funball.logic.determine_gameweek import determine_gameweek_no
+from fantasy_funball.logic.update_results import update_results
+
+if __name__ == "__main__":
+    """Restores results for all gameweeks"""
+    gameweek_no = determine_gameweek_no()
+    for _ in range(1, gameweek_no):
+        update_results(gameweek_no=gameweek_no)
