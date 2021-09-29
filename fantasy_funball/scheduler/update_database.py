bd2016d77352648537e39f4fd1a7383a79d6c7c8
p-ml <34091508+pjjml@users.noreply.github.com>
2021-09-29 17:30:12 +0100

    Refactor to allow check_choices to be run for prev gameweek

--- fantasy_funball/scheduler/update_database.py
+++ fantasy_funball/scheduler/update_database.py
@@ -1,5 +1,8 @@
 from fantasy_funball.helpers.date import determine_if_transfer_window
-from fantasy_funball.logic.check_choices import check_choices, check_lineups
+from fantasy_funball.logic.check_choices import (
+    check_choices_if_deadline_day,
+    check_lineups,
+)
 from fantasy_funball.logic.determine_gameweek import determine_gameweek_no
 from fantasy_funball.logic.update_results import update_results
 from fantasy_funball.logic.update_standings import update_standings
@@ -9,7 +12,7 @@ if __name__ == "__main__":
     # Run at midnight every day by Heroku Job Scheduler
     gameweek_no = determine_gameweek_no()
     if gameweek_no > 0:
-        check_choices(gameweek_no=gameweek_no)
+        check_choices_if_deadline_day(gameweek_no=gameweek_no)
         update_results(gameweek_no=gameweek_no)
         check_lineups(gameweek_no=gameweek_no)
         update_standings(gameweek_no=gameweek_no)
