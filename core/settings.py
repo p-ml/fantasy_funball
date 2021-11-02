b92a2929b870a1853f35b147f77892ceb1de9d0c
p-ml <34091508+pjjml@users.noreply.github.com>
2021-11-02 16:40:57 +0000

    Version bump to v1.2.1

--- core/settings.py
+++ core/settings.py
@@ -17,7 +17,7 @@ import django_heroku
 import sentry_sdk
 from sentry_sdk.integrations.django import DjangoIntegration
 
-VERSION = "v1.2.0"
+VERSION = "v1.2.1"
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
