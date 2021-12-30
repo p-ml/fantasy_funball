2a608b7b7887afad1f43bbd94a702bf6c47ecab9
p-ml <34091508+pjjml@users.noreply.github.com>
2021-12-30 11:40:05 +0000

    Debug scheduler, force it to run

--- fantasy_funball/scheduler/update_database.py
+++ fantasy_funball/scheduler/update_database.py
@@ -1,3 +1,4 @@
+import logging
 import os
 
 from fantasy_funball.logic.check_choices import (
@@ -10,20 +11,26 @@ from fantasy_funball.logic.update_results import update_results
 from fantasy_funball.logic.update_standings import update_standings
 from fantasy_funball.scheduler.update_players import update_players
 
+logger = logging.getLogger("papertrail")
+
+
 if __name__ == "__main__":
     # Run at midnight every day by Heroku Job Scheduler
 
     # Check if game is paused
     game_paused = os.environ.get("GAME_PAUSED")
-
     update_players()
     gameweek_no = determine_gameweek_no()
-    if gameweek_no > 0 and not game_paused:
-        check_choices_if_deadline_day(gameweek_no=gameweek_no)
-        update_results(gameweek_no=gameweek_no)
-        check_teams_and_lineups(gameweek_no=gameweek_no)
-        update_standings(gameweek_no=gameweek_no)
-        update_fixtures(gameweek_no=gameweek_no)
 
-    else:
-        print("Season has not started yet")
+    logger.info(f"Game paused: {game_paused}")
+    logger.info(f"Gameweek no: {gameweek_no}")
+
+    # if gameweek_no > 0 and not game_paused:
+    check_choices_if_deadline_day(gameweek_no=gameweek_no)
+    update_results(gameweek_no=gameweek_no)
+    check_teams_and_lineups(gameweek_no=gameweek_no)
+    update_standings(gameweek_no=gameweek_no)
+    update_fixtures(gameweek_no=gameweek_no)
+
+    # else:
+    #     print("Season has not started yet")
